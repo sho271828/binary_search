65cd9821fe99ec7635eb67edba24aebc0d3bca43
Nakamura Syo <nakamura.s.bi@is-mac15.edu.c.titech.ac.jp>
2019-06-20 18:19:41 +0900

    完成

--- src/spear.c
+++ src/spear.c
@@ -11,7 +11,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = A[n - 1];
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        int sum = 0;
+        for(i = 0; i < n; i++){
+            sum = sum + A[i] / m;
+        }
+        if(sum < k) ub = m;
+        else lb = m;
+    }
+    
+    printf("%d\n", lb);
   return 0;
 }
