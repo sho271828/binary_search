b94d4c4a42ef8e0c8b3b724aebc98576c4d2d38e
Nakamura Syo <nakamura.s.bi@is-mac15.edu.c.titech.ac.jp>
2019-06-21 15:58:08 +0900

    途中

--- src/works.c
+++ src/works.c
@@ -5,13 +5,29 @@ int k;
 int A[100000];
 
 
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    int sum = 0;
+    for(i = 0; i < n; i++){
+        sum += A[i];
+    }
+    lb = 0;
+    ub = sum;
+    while(ub - lb > 1){
+        int B[100000] = 0;
+        for(i = 0; i < n - n / k + 1; i++){
+            B[i] += A[i]
+        if(n / k = 1) ub = m;
+        else lb = m;
+    }
+    
+    
+    printf("%d\n", ub);
   return 0;
 }
