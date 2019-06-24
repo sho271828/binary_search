96cb0b3c2916c9abf1d1742ac0a1552fdca4a064
Nakamura Syo <nakamura.s.bi@is-mac15.edu.c.titech.ac.jp>
2019-06-24 12:55:48 +0900

    完成

--- src/works.c
+++ src/works.c
@@ -4,7 +4,23 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int m){
+    int i;
+    int need = 1;
+    int sum = 0;
+    for(i = 0; i < n; i++){
+        if(A[i] > m){
+            return 0;
+        }
+        sum += A[i];
+        if(sum > m){
+            sum = 0;
+            i--;
+            need++;
+        }
+    }
+    return need <= k;
+}
 
 
 int main(){
@@ -13,21 +29,20 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-    int sum = 0;
-    for(i = 0; i < n; i++){
-        sum += A[i];
-    }
     lb = 0;
-    ub = sum;
+    ub = 1000000001;
     while(ub - lb > 1){
-        int B[100000] = 0;
-        for(i = 0; i < n - n / k + 1; i++){
-            B[i] += A[i]
-        if(n / k = 1) ub = m;
-        else lb = m;
+        int m =(lb + ub) / 2;
+        if(p(m)){
+        ub = m;
+    }
+        else {
+            lb = m;
+        }
     }
     
     
     printf("%d\n", ub);
   return 0;
 }
+
